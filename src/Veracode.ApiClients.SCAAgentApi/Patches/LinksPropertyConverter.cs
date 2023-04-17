b8d40aa6b2d5f028d09e6a312f2fa85b84825979
cuteribs <85863433+cuteribs@users.noreply.github.com>
2023-04-17 14:19:31 +0800

    fix code smell

--- src/Veracode.ApiClients.SCAAgentApi/Patches/LinksPropertyConverter.cs
+++ src/Veracode.ApiClients.SCAAgentApi/Patches/LinksPropertyConverter.cs
@@ -9,25 +9,28 @@ namespace Veracode.ApiClients.SCAAgent.Api.Patches;
 
 public class LinksPropertyConverter : JsonConverter<IList<Link>>
 {
-	public override IList<Link>? ReadJson(JsonReader reader, Type objectType, IList<Link>? existingValue, bool hasExistingValue, JsonSerializer serializer)
+	public override IList<Link> ReadJson(JsonReader reader, Type objectType, IList<Link> existingValue, bool hasExistingValue, JsonSerializer serializer)
 	{
+		var list = new List<Link>();
+
 		switch (reader.TokenType)
 		{
 			case JsonToken.StartArray:
 				return JArray.Load(reader).ToObject<IList<Link>>();
 			case JsonToken.StartObject:
 				var dict = JObject.Load(reader).ToObject<IDictionary<string, Link>>();
-				return dict?.Select(p =>
+				list = dict?.Select(p =>
 				{
 					var link = p.Value;
 					link.Type = p.Key;
 					return link;
 				}).ToList();
+				break;
 		}
 
-		return default;
+		return list;
 	}
 
-	public override void WriteJson(JsonWriter writer, IList<Link>? value, JsonSerializer serializer)
+	public override void WriteJson(JsonWriter writer, IList<Link> value, JsonSerializer serializer)
 		=> throw new NotImplementedException();
 }
